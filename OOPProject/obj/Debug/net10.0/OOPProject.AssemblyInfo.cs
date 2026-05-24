b9fb0de309a9a537b434861fc512f721369c41cc
meerabwaheed <meerabdev@gmail.com>
2026-05-25 03:07:47 +0500

    code with class diagrams

--- OOPProject/obj/Debug/net10.0/OOPProject.AssemblyInfo.cs
+++ OOPProject/obj/Debug/net10.0/OOPProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OOPProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+259dfa705cb9cf79c215abe2deed9c4b3f109137")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d07d43296e68304afc24139b7560a2dc17452360")]
 [assembly: System.Reflection.AssemblyProductAttribute("OOPProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OOPProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
