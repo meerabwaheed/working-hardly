5e9c110150b3ec1fd7909e9994f553540475caa5
meerabwaheed <meerabdev@gmail.com>
2026-05-25 03:08:17 +0500

    un commented code here

--- OOPProject/Program.cs
+++ OOPProject/Program.cs
@@ -1,51 +1,51 @@
-﻿// using System;
-// class Animal { 
-//     public void eat()
-//     {
-//         Console.WriteLine("animal is eating");
-//     }
-// }
-// class dog : Animal
-// { 
-//     public void bark()
-//     {
-//         Console.WriteLine("dog is barking");
-//     }
-// }
-// class Program
-// { 
-//     static void Main()
-//     {
-//         dog d = new dog();
-//         d.bark();
-//         d.eat();
-//         Console.WriteLine("this code is written by meerab");
-//     }
-// }
+﻿using System;
+class Animal { 
+    public void eat()
+    {
+        Console.WriteLine("animal is eating");
+    }
+}
+class dog : Animal
+{ 
+    public void bark()
+    {
+        Console.WriteLine("dog is barking");
+    }
+}
+class Program
+{ 
+    static void Main()
+    {
+        dog d = new dog();
+        d.bark();
+        d.eat();
+        Console.WriteLine("this code is written by meerab");
+    }
+}
 
 
 
 
 
-                +----------------------+
-                |       Animal         |
-                +----------------------+
-                | - name : string      |
-                +----------------------+
-                | + Eat() : void       |
-                +----------------------+
-                           ▲
-                           |
-               -----------------------
-               |                     |
-               |                     |
-      +----------------+    +----------------+
-      |      Dog       |    |      Cat       |
-      +----------------+    +----------------+
-      | - breed:string |    | - color:string |
-      +----------------+    +----------------+
-      | + Bark() :void |    | + Meow() :void |
-      +----------------+    +----------------+
+    //             +----------------------+
+    //             |       Animal         |
+    //             +----------------------+
+    //             | - name : string      |
+    //             +----------------------+
+    //             | + Eat() : void       |
+    //             +----------------------+
+    //                        ▲
+    //                        |
+    //            -----------------------
+    //            |                     |
+    //            |                     |
+    //   +----------------+    +----------------+
+    //   |      Dog       |    |      Cat       |
+    //   +----------------+    +----------------+
+    //   | - breed:string |    | - color:string |
+    //   +----------------+    +----------------+
+    //   | + Bark() :void |    | + Meow() :void |
+    //   +----------------+    +----------------+
 
 using System;
 class Animal
